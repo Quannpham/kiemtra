e5824da5bc7a52ad4305067465ed1248445df119
Quannpham <phamquan170312@gmail.com>
2023-05-15 22:02:50 +0700

    chinh sua

--- bai1/script.js
+++ bai1/script.js
@@ -1,12 +1,12 @@
 function sumNguyenTo() {
 	
-    var a = parseInt($('#a').val());
-    var b = parseInt($('#b').val());
-    var sum = 0;
+    let a = parseInt($('#a').val());
+    let b = parseInt($('#b').val());
+    let sum = 0;
 
-    for (var i = a; i <= b; i++) {
-        var isPrime = true;
-        for (var j = 2; j <= Math.sqrt(i); j++) {
+    for (let i = a; i <= b; i++) {
+        let isPrime = true;
+        for (let j = 2; j <= Math.sqrt(i); j++) {
             if (i % j == 0) {
                 isPrime = false;
                 break;
